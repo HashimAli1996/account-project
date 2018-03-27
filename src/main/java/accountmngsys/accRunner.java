2d4492accb657f24b9f9ecda55526b22bec352a1
HashimAli1996 <hashim.ali@qa.com>
2018-03-27 17:03:23 +0100

    added test folder and files

--- src/main/java/accountmngsys/accRunner.java
+++ src/main/java/accountmngsys/accRunner.java
@@ -1,11 +1,5 @@
 package accountmngsys;
 
-import java.io.IOException;
-
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-
 public class accRunner {
 	
 	public static void main(String[] args) {
@@ -14,28 +8,16 @@ public class accRunner {
 		account firstAcc = new account(12345, "Hashim", "Ali");
 		account secAcc = new account(678910, "Abdi", "Rahman");
 		account thirdAcc = new account(11121314, "Jordan", "Smart");
-		account fthAcc = new account(15161718, "Sean", "Smart");
+		account fthAcc = new account(15161718, "Shaun", "Smart");
 		
 		service main = new service();
 		
 		main.insertAcc(firstAcc);
 		main.insertAcc(secAcc);
-		main.insertAcc(thirdAcc);
-		main.insertAcc(fthAcc);
+		//main.insertAcc(thirdAcc);
+		//main.insertAcc(fthAcc);
 		
-		ObjectMapper mapper = new ObjectMapper();
+		System.out.println(main.mapToJSON());
 		
-		try {
-			System.out.println(mapper.writeValueAsString(main.returnJSONMap()));
-		} catch (JsonGenerationException e) {
-			System.out.println("JsonGenerationException");
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			System.out.println("JsonMappingException");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("IOException");
-			e.printStackTrace();
-		}
 	}
 }
--- src/main/java/accountmngsys/service.java
+++ src/main/java/accountmngsys/service.java
@@ -1,41 +1,64 @@
 package accountmngsys;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
 public class service {
-	
+
 	private HashMap<Integer, account> accMap = new HashMap<Integer, account>();
-	
+
 	public account retrieveAcc(int accNum) {
-	
+
 		return accMap.get(accNum);
-		
+
 	}
-	
+
 	public String insertAcc(account newAcc) {
 		accMap.put(newAcc.getAccnum(), newAcc);
 		return "Account: " + newAcc.getAccnum() + " added!";
 	}
-	
+
 	public String removeAcc(int accNum) {
 		accMap.remove(accNum);
 		return "Account: " + accNum + " removed!";
 	}
-	
+
 	public void printMap() {
-		
-		for (int acc: accMap.keySet()) {
-			
+
+		for (int acc : accMap.keySet()) {
+
 			String value = accMap.get(acc).toString();
-			
+
 			System.out.println(value);
-			
+
 		}
 	}
-	
-	public Map<Integer, account> returnJSONMap(){
+
+	public Map<Integer, account> returnMap() {
 		return accMap;
 	}
-	
+
+	public String mapToJSON() {
+		ObjectMapper jsonMaker = new ObjectMapper();
+		try {
+			return jsonMaker.writeValueAsString(accMap);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+			return "error";
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+			return "error";
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "error";
+		}
+
+	}
+
 }
